cc9b42497bb578dd7d929b306aeac0400150846b
muhanad <mouhandmk1@gmail.com>
2021-05-05 02:34:34 +0200

    refaire de page dynamique

--- js/main.js
+++ js/main.js
@@ -1,74 +1,60 @@
-const api = "http://localhost:3000/api/cameras";
+// ----------------------Déclaration des variables ---------------------
+let i = [];
+let _id = []
+let lenses = [];
+let name = [];
+let price = [];
+let description = [];
+let imageUrl = [];
+let structureProdoites = "";
+// ------------Récupérer des donnes api -------------
 
+const api = "http://localhost:3000/api/cameras";
 async function getData(){
   const response = await fetch(api)
   const data = await response.json();
-  printData(data)
+  AaffichageProduite(data)
 }
+// ------------------Fonction pour afficher les donnes automatismes -------------
 
-function printData(data){
- const cardPage = document.querySelector(".importJs");
- cardPage.innerHTML = `
-  <div class="container">
-  <div class="row">
-    <div class="mt-5 col -sm-12 col-lg col-md-12">
-      <div class="card shadow p-3 mb-5 bg-white rounded">
-      <img src="${data[0].imageUrl}" class="img4 card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${data[0].name}</h5>
-          <p class="card-text">${data[0].description}</p>
-          <a href="../htmlPage/zurs.html" class="btn btn-primary">découvrir</a>
-        </div>
-      </div>
-    </div>
-    <div class="mt-5 col-sm-12 col-lg col-md-12">
-      <div class="card shadow p-3 mb-5 bg-white rounded">
-        <img src="${data[1].imageUrl}" class="img2 card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${data[1].name}</h5>
-          <p class="card-text">${data[1].description}</p>
-          <a href="#" class="btn btn-primary">découvrir</a>
-        </div>
-      </div>
-    </div>
-    <div class="mt-5 col-sm-12 col-lg col-md-12">
-      <div class="card shadow p-3 mb-5 bg-white rounded">
-        <img src="${data[2].imageUrl}" class="img3 card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${data[2].name}</h5>
-          <p class="card-text">${data[2].description}</p>
-          <a href="#" class="btn btn-primary">découvrir</a>
-        </div>
-      </div>
-    </div>
-  </div>
- </div>
- <div class="container">
-  <div class="row"> 
-    <div class="mt-5 col-sm-12 col-lg col-md-12">
-      <div class="card shadow p-3 mb-5 bg-white rounded">
-        <img src="${data[3].imageUrl}" class="img4 card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${data[3].name}</h5>
-          <p class="card-text">${data[3].description}</p>
-          <a href="#" class="btn btn-primary">découvrir</a>
-        </div>
-      </div>
-    </div>
-    <div class="mt-5 col-sm-12 col-lg col-md-12">
-      <div class="card shadow p-3 mb-5 bg-white rounded">
-        <img src="${data[4].imageUrl}" class="img5 card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${data[4].name}</h5>
-          <p class="card-text">${data[4].description}consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
-          <a href="#" class="btn btn-primary">découvrir</a>
-        </div>
-      </div>
-    </div>
-  </div>
+function AaffichageProduite(data){
 
- `
+//  ----Positon mes produite -----
+
+const PositonMesProduite = document.querySelector(".MesProduite");
+//----la boucle for por afichage MesProduite
+  for (let i = 0; i < data.length; i++) {
+
+    // mettre les donnes dans des variables ----
+
+    data.forEach ((element, i) => {
+      _id[i] = element._id;
+      name[i] = element.name;
+      price[i] = element.price;
+      description[i] = element.description;
+      imageUrl[i] = element.imageUrl;
+    });
+    
+    // ---------------afficher les donnes sur la page------------------------
+    
+  structureProdoites += `
+    <a class="text-decoration-none text-dark" href="../htmlPage/prouditeSelection.html?${_id[i]}">
+      <div class="mt-5 col-sm-12 col-lg col-md-12">
+        <div class="card shadow p-3 mb-5 bg-white rounded">
+          <img src="${imageUrl[i]}" class="img5 card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${name[i]}</h5>
+            <p class="card-text">${description[i]}consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+            <button class="btn btn-primary mt-2" type="submit">découvrir</button>
+          </div>
+        </div>  
+      </div>
+    </a>    
+  `
+  PositonMesProduite.innerHTML = structureProdoites;
+  // PositonMesProduite02.innerHTML = structureProdoites;
+
+  }
 }
 
 getData();
-
--- js/produite.js
+++ js/produite.js
@@ -1,39 +1,38 @@
+// -------------------Récupération de la chaine de requête dans l’url-----------
+const requeteChaine_url_id = window.location.search;
+const leId = requeteChaine_url_id.slice(1);
+// ------------Récupérer des donnes api -----------
+let api01 = `http://localhost:3000/api/cameras/${leId}`;
+// -------Affichage des produites à partir des leurs id
+async function getDataProduites(){
+  const response = await fetch(api01)
+  const data = await response.json(); 
+  console.log(leId);
 
-const api = "http://localhost:3000/api/cameras";
-
-async function getData(){
-  const response = await fetch(api)
-  const data = await response.json();
-  printData(data)
 }
+getDataProduites();
 
-function printData(data){
- const cardPage = document.querySelector(".Produited");
- cardPage.innerHTML = `
- <div class="container">
-  <div class="row">
-  <div class="col-1"></div>
-    <div class="mt-5 col-sm-12 col-lg-9">
-      <div class="card shadow p-3 mb-5 bg-white rounded">
-      <img src="${data[0].imageUrl}" class="img4 card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${data[0].name}</h5>
-          <p class="card-text">${data[0].description}</p>
-          <select class="mt-2 btn btn-primary" name="" id="">
-            <option class="text-white" value="">
-            ${data[0].lenses[0]}
-            </option>
-            <option class="text-white" value="">
-            ${data[0].lenses[1]}
-            </option>
-        </select>
-        <button class="btn btn-primary mt-2" type="submit">ajouter au panier</button>
-        </div>
-      </div>
+
+// ------------------Affichage des produites à partir des leurs id---------
+
+// const idProduiteSelector = data.find((element) => element._id === _id);
+// console.log(idProduiteSelector);
+
+const potistionElement = document.querySelector(".Produited");
+
+const structureProdoite02 = `
+<a class="text-decoration-none text-dark" href="../htmlPage/prouditeSelection.html?${_id[i]}">
+<div class="mt-5 col-sm-12 col-lg col-md-12">
+  <div class="card shadow p-3 mb-5 bg-white rounded">
+    <img src="${imageUrl[i]}" class="img5 card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${name[i]}</h5>
+      <p class="card-text">${description[i]}consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+      <button class="btn btn-primary mt-2" type="submit">découvrir</button>
     </div>
-    <div class="col-1"></div>
-  </div>
-</div> 
- `
-}
- getData();
+  </div>  
+</div>
+</a>    
+`;
+
+potistionElement.innerHTML = structureProdoite02;
