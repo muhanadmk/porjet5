2b8c7a1ac8985152e22d8332c3091ed79c1fc412
muhanad <mouhandmk1@gmail.com>
2021-05-21 15:06:00 +0200

    valider w3c html

--- js/confromation.js
+++ js/confromation.js
@@ -11,26 +11,24 @@ const formulaireDansLeLocalStorage = JSON.parse(localStorage.getItem("formulaire
 let structureConformationsHTML = [];
 const potistionConformation = document.querySelector("#confromation");
 
-
   structureConformationsHTML = 
-  structureConformationsHTML +
    `
-   <div class="row">
-      <div class="col-2"></div>
-      <div class="card text-center col-8 shadow p-3 bg-white rounded">
-        <div class="card-body">
-          <h4 class="card-title">l'équipe de notre site vous remercie et vous souhaite une excellente bonne journée</h4>
-            <h5 class="card-text text-success">le numeruo de commande est : ${IdServer}</h5>
-          <h5 class="card-text mt-20 text-primary">monsieur ${formulaireDansLeLocalStorage.lastName} votre commande sera envoyé dans la semaine suivante à l'adresse: <br />  ${formulaireDansLeLocalStorage.address} à la ville de ${formulaireDansLeLocalStorage.city} </h5>
-          <h5 class="card-text text-danger">le pirx totla de vos achete est :</h5>
-          <h4 class="card-text text-danger">${prixtotal} €</h4>
-          <h6 class="card-text mt-30"> Notre Protection de l'acheteur vous protège du premier clic à la livraison.</h6>
-          <h6>A toute heure, vous pouvez contacter le Centre d'Aide pour une expérience d'achat facile.</h6>
-          <h6>Payez avec les méthodes de paiement les plus populaires et les plus s?res au monde.</h6>
-          <a href="../porjet5/index.html" class="btn btn-primary mt-2">fermer</a>
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="card text-center col-8 shadow p-3 bg-white rounded">
+          <div class="card-body">
+            <h4 class="card-title">l'équipe de notre site vous remercie et vous souhaite une excellente bonne journée</h4>
+              <h5 class="card-text text-success">le numeruo de commande est : ${IdServer}</h5>
+            <h5 class="card-text mt-20 text-primary">monsieur ${formulaireDansLeLocalStorage.lastName} votre commande sera envoyé dans la semaine suivante à l'adresse: <br />  ${formulaireDansLeLocalStorage.address} à la ville de ${formulaireDansLeLocalStorage.city} </h5>
+            <h5 class="card-text text-danger">le pirx totla de vos achete est :</h5>
+            <h4 class="card-text text-danger">${prixtotal} €</h4>
+            <h6 class="card-text mt-30"> Notre Protection de l'acheteur vous protège du premier clic à la livraison.</h6>
+            <h6>A toute heure, vous pouvez contacter le Centre d'Aide pour une expérience d'achat facile.</h6>
+            <h6>Payez avec les méthodes de paiement les plus populaires et les plus s?res au monde.</h6>
+            <a href="../porjet5/index.html" class="btn btn-primary mt-2" id="click">fermer</a>
+          </div>
         </div>
-      </div>
-    </div>  
+      </div>  
   `;
   potistionConformation.innerHTML = structureConformationsHTML;
 
